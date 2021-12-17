85f0b925709ddaecf91c7f5dae0c19adaf4fe1b5
timurlalala <timursamwork@gmail.com>
2021-12-17 06:36:28 +0300

    bug fix

--- app/handlers/service_commands.py
+++ app/handlers/service_commands.py
@@ -1,14 +1,22 @@
 from aiogram import types, Dispatcher
-from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import FSMContext, filters
 import logging
 from app.app import active
 
 
-async def cmd_start(message: types.Message, state: FSMContext):
+async def cmd_start_pm(message: types.Message, state: FSMContext):
+    logging.info(f'{message.chat.id} --- {message.from_user.username}')
+    await state.finish()
+    await message.answer(f"Привет, {message.chat.full_name}! Я Абобус.",
+                         reply_markup=types.ReplyKeyboardRemove())
+
+
+async def cmd_start_group(message: types.Message, state: FSMContext):
     active.check_model_exists(message.chat.id)
     logging.info(f'{message.chat.id} --- {message.from_user.username}')
     await state.finish()
-    await message.answer(f"Привет, {message.chat.full_name}! Я Абобус.", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(f"Привет, {message.chat.full_name}! Я Абобус.",
+                         reply_markup=types.ReplyKeyboardRemove())
 
 
 async def cmd_cancel(message: types.Message, state: FSMContext):
@@ -21,6 +29,7 @@ async def cmd_help(message: types.Message):
 
 
 def register_commands_handlers(dp: Dispatcher):
-    dp.register_message_handler(cmd_start, commands='start', state="*")
+    dp.register_message_handler(cmd_start_pm, commands='start', state="*")
+    dp.register_message_handler(cmd_start_group, commands='start', state="*")
     dp.register_message_handler(cmd_cancel, commands='cancel', state="*")
     dp.register_message_handler(cmd_help, commands='help')
